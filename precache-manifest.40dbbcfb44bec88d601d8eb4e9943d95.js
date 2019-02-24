054adecaa13f3d1043a9f6a958af74fa2fc8d8d4
Anastasiya <33834629+375291601334@users.noreply.github.com>
2019-02-24 19:53:23 +0300

    Add files via upload

---
+++ precache-manifest.40dbbcfb44bec88d601d8eb4e9943d95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d85d155d533f895ba81a",
+    "url": "/static/js/main.d85d155d.chunk.js"
+  },
+  {
+    "revision": "d4bc1337f67a6d49373a",
+    "url": "/static/js/1.d4bc1337.chunk.js"
+  },
+  {
+    "revision": "d85d155d533f895ba81a",
+    "url": "/static/css/main.fe711522.chunk.css"
+  },
+  {
+    "revision": "d4bc1337f67a6d49373a",
+    "url": "/static/css/1.f60508b6.chunk.css"
+  },
+  {
+    "revision": "4b29db093dff795f2cde4ecb4a17cee6",
+    "url": "/index.html"
+  }
+];
